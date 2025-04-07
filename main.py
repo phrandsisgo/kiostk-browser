34926a6311d231901031a44c3d32cc4740bedeb3
Francisco Wohlgemuth <75257353+phrandsisgo@users.noreply.github.com>
2025-04-07 14:15:52 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+import subprocess
+import time
+import pyautogui
+import pygetwindow as gw
+
+# 1. Starte Firefox im Kiosk-Modus (für Touchscreen)
+subprocess.Popen([
+    r"C:\Program Files\Mozilla Firefox\firefox.exe",
+    "--kiosk",
+    "https://seite-fuer-touchscreen.de"
+])
+
+# 2. Warte ein paar Sekunden
+time.sleep(3)
+
+# 3. Starte ein zweites Firefox-Fenster (für Monitor)
+subprocess.Popen([
+    r"C:\Program Files\Mozilla Firefox\firefox.exe",
+    "--new-window",
+    "https://seite-fuer-monitor.de"
+])
+
+# 4. Warte, bis das Fenster geladen ist
+time.sleep(7)
+
+# 5. Aktiviere das Fenster und drücke F11
+windows = gw.getWindowsWithTitle("seite-fuer-monitor")  # Passe den Tab-Titel an
+if windows:
+    win = windows[0]
+    win.activate()
+    time.sleep(1)
+    pyautogui.press('f11')
+else:
+    print("Fenster nicht gefunden")
+
+# Optional: Fenster gezielt auf Monitor 2 verschieben
+# Beispiel: win.moveTo(x=1920, y=0)  # Falls Monitor 2 rechts ist
